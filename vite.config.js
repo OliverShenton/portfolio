b160fbefd418298c5620cda1a180f61d4b2f7baf
olive <olivermshenton@hotmail.com>
2025-01-19 23:16:24 +0000

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import ghPages from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/", // Use "/" if deploying to olivershenton.github.io
-  plugins: [react(), ghPages()],
+  base: "/",
+  plugins: [react()],
 });
