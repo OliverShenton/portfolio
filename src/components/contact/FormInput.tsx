7c93089dea5efc12ec22cd4401808f4c976762c5
olive <olivermshenton@hotmail.com>
2025-07-24 01:34:17 +0100

    Error fixes for Vercel deployment part 3

--- src/components/contact/FormInput.tsx
+++ src/components/contact/FormInput.tsx
@@ -2,16 +2,17 @@ type FormInputProps = {
   id: string;
   placeholder: string;
   type?: string;
-  // value: string;
+  value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const FormInput = ({ id, type = "text", onChange, placeholder }: FormInputProps) => {
+const FormInput = ({ id, type = "text", value, onChange, placeholder }: FormInputProps) => {
   return (
     <input
       id={id}
       type={type}
       name={id}
+      value={value}
       onChange={onChange}
       placeholder={placeholder}
       className="p-4 border flex-1"
